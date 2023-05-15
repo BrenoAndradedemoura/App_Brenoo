805a35b0aad77f2d96099aca871b100a37f9897d
BrenoAndradedemoura <brenomourasandrade@gmail.com>
2023-05-15 10:28:49 -0300

    Tudo certinho

--- src/navigations/drawer.navigation.tsx
+++ src/navigations/drawer.navigation.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { DrawerNavigationProp, createDrawerNavigator } from '@react-navigation/drawer';
-import { ScreenCadastrar, ScreenLogin, ScreenPerfil } from "../screens";
+import { ScreenCadastrar, ScreenCamera, ScreenLogin, ScreenPerfil } from "../screens";
 import { colors } from "../styles/colors"; 
-import { Ionicons } from '@expo/vector-icons'; 
+import { AntDesign, Ionicons } from '@expo/vector-icons'; 
 import { StackActions } from '@react-navigation/native';
 type DrawerParamList = {
   Perfil: undefined
+  Camera: undefined
 }
 type DrawerScreenNavigationProp = DrawerNavigationProp<DrawerParamList, 'Perfil'>
 export type DrawerTypes = {
@@ -21,7 +22,22 @@ export type DrawerTypes = {
       },
       drawerActiveTintColor: colors.white
     }}>
-      <Drawer.Screen name="Perfil" component={ScreenPerfil} />
+      <Drawer.Screen name="Perfil" component={ScreenPerfil}
+        options={{
+          drawerIcon: () => (
+            <Ionicons name='person' size={24} color={colors.white} />
+          )
+        }}
+      
+      />
+      <Drawer.Screen name="Camera" component={ScreenCamera}
+       options={{
+        drawerIcon: () => (
+          <AntDesign name='camera' size={24} color={colors.white} />
+        )
+      }}
+      
+      />
     </Drawer.Navigator>
   );
 }
--- src/navigations/tab.navigations.tsx
+++ src/navigations/tab.navigations.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BottomTabNavigationProp, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ScreenPerfil } from "../screens";
+import { ScreenCamera, ScreenPerfil } from "../screens";
 import { colors } from "../styles/colors"; 
-import { Ionicons } from '@expo/vector-icons'; 
+import { Ionicons, AntDesign } from '@expo/vector-icons'; 
 type TabParamList = {
   Perfil: undefined
+  Camera: undefined
 }
 type TabScreenNavigationProp = BottomTabNavigationProp<TabParamList, 'Perfil'>
 export type TabTypes = {
@@ -31,6 +32,13 @@ export type TabTypes = {
           )
         }}
       />
+      <Tab.Screen name="Camera" component={ScreenCamera}
+      options={{
+        tabBarIcon: () => (
+          <AntDesign name='camera' color={colors.white} size={24}  />
+        )
+      }} />
+     
     </Tab.Navigator>
   );
 }
