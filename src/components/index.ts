1225da9a06272f4e72b2613049200eb3ae08ce1e
BrenoAndradedemoura <brenomourasandrade@gmail.com>
2023-08-28 10:44:53 -0300

    ok

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export { TitleSlider as ComponentTitleSlider } from "./TitleSlider"
 export { ListMarker as ComponentListMarker } from "./ListMarker"
 export { ButtonSlider as ComponentButtonSlider } from "./ButtonSlider"
 export { ButtonInterface as ComponentButtonInterface } from "./ButtonInterface"
+export { Loading as ComponentLoading } from "./loading"
 
 
--- src/screens/Cadastrar/index.tsx
+++ src/screens/Cadastrar/index.tsx
@@ -1,17 +1,65 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { View, Text,
-     KeyboardAvoidingView
+     KeyboardAvoidingView,
+     Alert
     } from "react-native"
 import { AntDesign, MaterialIcons } from '@expo/vector-icons'
 import { styles } from "./styles"
 import { TextInput } from "react-native-gesture-handler"
 import { colors } from "../../styles/colors"
 import { FontAwesome5 } from '@expo/vector-icons'; 
-import { ComponentButtonInterface }  from "../../components"
+import { ComponentButtonInterface, ComponentLoading }  from "../../components"
 import { LoginTypes } from "../../navigations/login.navigation";
+import { IRegister } from "../../services/data/User"
+import { AxiosError } from "axios"
+import { apiUser } from "../../services/data"
+export interface IErrorApi {
+    errors: {
+        rule: string
+        field: string
+        message: string
+    }[]
+}
 
 export function Cadastrar({navigation}: LoginTypes) {
+    const [data, setData ] = useState<IRegister>()
+    const [isLoading, setISLoading] = useState(true)
+    function handleChange(item: IRegister ) {
+        setData({...data, ...item})
+    }
+    async function handleRegister() {
+        try {
+            setISLoading(true)
+            if(data?.name && data.email && data.password) {
+                const response =  await apiUser.register(data)
+                Alert.alert(`${response.data.name} cadastrado!!!`)
+                setISLoading(false)
+                navigation.navigate('Login')
+            } else {
+                Alert.alert("Preencha todos os campos!!!")
+            }
+        } catch (error) {
+            const err = error as AxiosError
+            const errorData = err.response?.data as IErrorApi
+            let message = ""
+            if (errorData) {
+                for (const iterator of errorData.errors) {
+                    message = `${message} ${iterator.message} \n`
+
+                }
+            }
+        }
+    }
+    useEffect(()=> {
+        setTimeout(() => {
+            setISLoading(false)
+        }, 500)
+    }, [])
     return (
+    <>
+        {isLoading ? (
+         <ComponentLoading />
+     ) : (
         <View style={styles.container}>
         <KeyboardAvoidingView>
             <Text style={styles.title}> Cadastre-se </Text>
@@ -19,6 +67,7 @@ export function Cadastrar({navigation}: LoginTypes) {
             <AntDesign name="user" style={styles.icon} />
                     <TextInput
                         placeholder="Nome"
+                        onChangeText={(i) => handleChange({name: i})}
                         placeholderTextColor={colors.thirdLight}
                         autoCapitalize="none"
                         style={styles.input}
@@ -28,6 +77,7 @@ export function Cadastrar({navigation}: LoginTypes) {
                     <MaterialIcons name="email" style={styles.icon} />
                     <TextInput
                         placeholder="Email"
+                        onChangeText={(i) => handleChange({email: i})}
                         placeholderTextColor={colors.thirdLight}
                         keyboardType="email-address"
                         autoCapitalize="none"
@@ -38,15 +88,19 @@ export function Cadastrar({navigation}: LoginTypes) {
                     <FontAwesome5 name="key" style={styles.icon} />
                     <TextInput
                         placeholder="Senha"
+                        onChangeText={(i) => handleChange({password: i})}
                         placeholderTextColor={colors.thirdLight}
                         secureTextEntry={true}
                         autoCapitalize="none"
                         style={styles.input}
                     />
             </View>
+        <ComponentButtonInterface title="Salvar" type="primary" onPressI={handleRegister} /> 
         <ComponentButtonInterface title="Entrar" type="primary" onPressI={()=> {navigation.navigate('Drawer')}} /> 
         <ComponentButtonInterface title="Voltar" type="third" onPressI={()=> { navigation.navigate('Login')}} />  
         </KeyboardAvoidingView>       
         </View>
-    )
+    )}
+    </>
+    );
 }
